cb75870292ac73059c8793d3dba22989f61dff30
LizzBricce <bricce.e@aluno.ifsp.edu.br>
2024-08-25 16:33:16 -0300

    refatoracao de classes relacionadas ao login

--- src/main/java/doo/gym/academyproject/B_useCases/dto/LoginDTO.java
+++ src/main/java/doo/gym/academyproject/B_useCases/dto/LoginDTO.java
@@ -4,7 +4,9 @@ public class LoginDTO {
     private String email;
     private String password;
 
-    public LoginDTO (String email, String password) {
+    public LoginDTO() {}
+
+    public LoginDTO(String email, String password) {
         this.email = email;
         this.password = password;
     }
